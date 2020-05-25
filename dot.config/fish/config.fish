742ebaf763e2e809a58fd3826879e78ad1b7befd
Daichi GOTO <daichigoto@gmail.com>
2020-05-25 18:51:42 +0900

    update

--- dot.config/fish/config.fish
+++ dot.config/fish/config.fish
@@ -72,12 +72,15 @@ switch $TERM
 case 'xterm*' 'vt*'
 	set --export LSCOLORS cxFxCxdxBxegedabagacad
 	set --export LS_COLORS 'di=32:ln=35:so=01:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+	set --export CLICOLOR
 case 'cons25'
 	set --export LSCOLORS cxFxCxdxBxegedabagacad
 	set --export LS_COLORS 'di=01;32:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+	set --export CLICOLOR
 case '*'
 	set --export LSCOLORS cxFxCxdxBxegedabagacad
 	set --export LS_COLORS 'di=01;32:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+	set --export CLICOLOR
 end
 
 alias less="less -X"
